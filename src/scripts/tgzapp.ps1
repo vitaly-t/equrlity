a6ecc0b1ef689589df7a3f2ffc7425c83cce1713
Gary Stephenson <gary@oxide.net.au>
2017-02-13 07:33:37 +1100

    just to shut git up...

---
+++ src/scripts/tgzapp.ps1
@@ -0,0 +1,7 @@
+$source = "./dist/capuchin"
+$destination = "./assets/synereo-plugin.tar.gz"
+
+set-alias sz "D:\dev\tools\7za.exe"  
+
+sz a -ttar temp.tar $source/*
+sz a -tgzip $destination temp.tar
