3206852c7b5d7e3d1ed0da9e54286af762d3fcd7
Gary Stephenson <gary@oxide.net.au>
2017-02-23 03:21:55 +1100

    remove spurious jwt.

--- src/capuchin/settings.tsx
+++ src/capuchin/settings.tsx
@@ -44,39 +44,77 @@ export class SettingsPage extends React.Component<SettingsPageProps, SettingsPag
   render() {
     let st = this.props.appState;
     let invs = st.investments;
-    let invrows = invs.map(l => {
-      let linkId = l.linkId;
-      let url = l.contentUrl;
-      let redeem = () => { chrome.runtime.sendMessage({ eventType: "RedeemLink", linkId, async: true }); };
-      let onclick = () => { chrome.tabs.create({ active: true, url }); };
+    let invdiv = <p>You have no current investments</p>
+    if (invs.length > 0) {
+      let invrows = invs.map(l => {
+        let linkId = l.linkId;
+        let url = l.contentUrl;
+        let redeem = () => { chrome.runtime.sendMessage({ eventType: "RedeemLink", linkId, async: true }); };
+        let onclick = () => { chrome.tabs.create({ active: true, url }); };
 
-      return (
-        <tr key={l.linkId} >
-          <td><button onClick={redeem}>Redeem</button></td>
-          <td><a href="" onClick={onclick} >{url}</a></td>
-          <td>{l.linkDepth}</td>
-          <td>{l.promotionsCount}</td>
-          <td>{l.deliveriesCount}</td>
-          <td>{l.viewCount}</td>
-          <td>{l.amount}</td>
-        </tr>
+        return (
+          <tr key={l.linkId} >
+            <td><button onClick={redeem}>Redeem</button></td>
+            <td><a href="" onClick={onclick} >{url}</a></td>
+            <td>{l.linkDepth}</td>
+            <td>{l.promotionsCount}</td>
+            <td>{l.deliveriesCount}</td>
+            <td>{l.viewCount}</td>
+            <td>{l.amount}</td>
+          </tr>
+        );
+      });
+      invdiv = (
+        <table className="pt-table pt-striped pt-bordered" >
+          <thead>
+            <tr>
+              <th></th>
+              <th>Content</th>
+              <th>Depth</th>
+              <th>Amplifications</th>
+              <th>Deliveries</th>
+              <th>Views</th>
+              <th>Balance</th>
+            </tr>
+          </thead>
+          <tbody>
+            {invrows}
+          </tbody>
+        </table>
       );
-    });
+    }
 
     let links = st.promotions;
-    let linkrows = links.map(url => {
-      let dismiss = () => { chrome.runtime.sendMessage({ eventType: "DismissPromotion", url }); };
-      let onclick = () => { chrome.tabs.create({ active: true, url }); };
-      let [tgt, desc] = url.split('#');
-      if (desc) desc = desc.replace('_', ' ');
-      return (
-        <tr key={url} >
-          <td><button onClick={dismiss}>Dismiss</button></td>
-          <td><a href="" onClick={onclick} >{tgt}</a></td>
-          <td>{desc}</td>
-        </tr>
+    let linkdiv = <p>There are no new amplified links for you.</p>
+    if (links.length > 0) {
+      let linkrows = links.map(url => {
+        let dismiss = () => { chrome.runtime.sendMessage({ eventType: "DismissPromotion", url }); };
+        let onclick = () => { chrome.tabs.create({ active: true, url }); };
+        let [tgt, desc] = url.split('#');
+        if (desc) desc = desc.replace('_', ' ');
+        return (
+          <tr key={url} >
+            <td><button onClick={dismiss}>Dismiss</button></td>
+            <td><a href="" onClick={onclick} >{tgt}</a></td>
+            <td>{desc}</td>
+          </tr>
+        );
+      });
+      linkdiv = (
+          <table className="pt-table pt-striped pt-bordered">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Link</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              {linkrows}
+            </tbody>
+          </table>
       );
-    });
+    }
 
     let vsp = <div style={{ height: 20 }} />;
 
@@ -86,7 +124,7 @@ export class SettingsPage extends React.Component<SettingsPageProps, SettingsPag
         {vsp}
         <h5>Your Settings:</h5>
         <div style={{ width: '100%', marginTop: 5, marginLeft: 5, padding: 6 }}>
-          <div style={{ width: '20%' }} >NickName: </div>
+          <div style={{ width: '20%' }} >Nickname: </div>
           <input type="text" style={{ width: '60%' }} name="NickName" id="nickId" ref={(e) => this.ctrls.nickNameInput = e}
             value={this.state.nickName} onChange={(e) => this.changeNickName()} />
         </div>
@@ -109,39 +147,13 @@ export class SettingsPage extends React.Component<SettingsPageProps, SettingsPag
           {vsp}
           <h6>Your Current Wallet Balance is : {st.ampCredits}.</h6>
           {vsp}
-          <table className="pt-table pt-striped pt-bordered" >
-            <thead>
-              <tr>
-                <th></th>
-                <th>Content</th>
-                <th>Depth</th>
-                <th>Amplifications</th>
-                <th>Deliveries</th>
-                <th>Views</th>
-                <th>Balance</th>
-              </tr>
-            </thead>
-            <tbody>
-              {invrows}
-            </tbody>
-          </table>
+          {invdiv}
         </div>
         {vsp}
         <div>
           <h4>Amplified Links for you : </h4>
           {vsp}
-          <table className="pt-table pt-striped pt-bordered">
-            <thead>
-              <tr>
-                <th></th>
-                <th>Link</th>
-                <th>Description</th>
-              </tr>
-            </thead>
-            <tbody>
-              {linkrows}
-            </tbody>
-          </table>
+          {linkdiv}
         </div>
       </div>);
   }
